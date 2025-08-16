cdd307d0d1e54ef3d45509126d0e2deeae1e8bab
Jezebel1990 <jezebelguedes@gmail.com>
2025-08-16 00:07:37 -0300

    feat: create utils

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 import { getPlayerService } from "../services/players-service";
 
 export const getPlayer = async (req: Request, res: Response) => {
-    const data = getPlayerService();
-    res.status(200).json({ player: "messi"});
-;}
+    const httpResponse = await getPlayerService();
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+};
