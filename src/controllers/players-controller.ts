5d40b1b7062dd9a60863654f3a329b2f19ff17c2
Jezebel1990 <jezebelguedes@gmail.com>
2025-08-15 23:09:43 -0300

    feat: create services

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { getPlayerService } from "../services/players-service";
 
-export const getPlayer = (req: Request, res: Response) => {
+export const getPlayer = async (req: Request, res: Response) => {
+    const data = getPlayerService();
     res.status(200).json({ player: "messi"});
 ;}
