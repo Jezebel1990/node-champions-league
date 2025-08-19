b1baf071f20732839dac25f77e162dc2b6738a4e
Jezebel1990 <jezebelguedes@gmail.com>
2025-08-19 01:12:40 -0300

    feat: read the json file

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,12 +1,21 @@
 import { ClubModel } from "../models/club-model";
+import fs from "fs/promises";
+import path from "path";
 
-const database = [
-    {
-      id: 1,
-      name: "Real Madrid",
-    },
-];
+const clubsFilePath = path.resolve(__dirname, "../data/clubs.json");
 
-export const findAllClubs = async (): Promise<ClubModel[]>=> {
-    return database;
-}
+export const findAllClubs = async (): Promise<ClubModel[]> => {
+  try {
+    const data = await fs.readFile(clubsFilePath, "utf-8");
+    const clubs: ClubModel[] = JSON.parse(data);
+
+    if (!Array.isArray(clubs)) {
+      throw new Error("Formato inválido de dados no arquivo clubs.json");
+    }
+
+    return clubs;
+  } catch (error) {
+    console.error("Erro ao carregar clubes:", error);
+    return []; // Retorna lista vazia como fallback
+  }
+};
