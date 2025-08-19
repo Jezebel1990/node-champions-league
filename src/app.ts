6863bb4e142039455e80bd65a1c8ccd28006b113
Jezebel1990 <jezebelguedes@gmail.com>
2025-08-19 01:36:57 -0300

    feat: implement cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 function createApp() {
 const app = express();
 
 app.use(express.json());
 app.use("/api", router);
+
+//  const corsOptions = {
+//     origin: ["http://jezebel.sistem.com", "http://gov.br"],
+//     methods: ["GET", "UPDATE"],
+//   };
+
+// app.use(cors(corsOptions));
+app.use(cors());
 
  return app;
 }
